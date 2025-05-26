9645892e5716354054d9965fe5623ce56b95995e
K4V33R <30440787+kvr24611@users.noreply.github.com>
2025-05-26 15:12:40 +0545

    Add an API Test spec file

---
+++ tests/api_tests.spec.js
@@ -0,0 +1,76 @@
+import { test, expect } from '@playwright/test';
+
+test('API GET Request', async ({ request }) => {
+
+    const response = await request.get('https://reqres.in/api/users/2', {
+        headers: {
+            "x-api-key": "reqres-free-v1"
+        }
+    });
+
+    expect(response.status()).toBe(200);
+
+    const text = await response.text();
+
+    expect(text).toContain('Janet');
+
+    console.log(await response.json());
+
+})
+
+test('API POST Request', async ({ request }) => {
+
+    const response = await request.post('https://reqres.in/api/users', {
+        headers: {
+            "x-api-key": "reqres-free-v1"
+        },
+        data: {
+            "name": "morpheus",
+            "job": "leader"
+        }
+    });
+
+    expect(response.status()).toBe(201);
+
+    const text = await response.text();
+
+    expect(text).toContain('morpheus');
+
+    console.log(await response.json());
+
+
+})
+
+test('API PUT Request', async ({ request }) => {
+
+    const response = await request.put('https://reqres.in/api/users/2', {
+        headers: {
+            "x-api-key": "reqres-free-v1"
+        },
+        data: {
+            "name": "morpheus",
+            "job": "zion resident" 
+        }
+    });
+
+    expect(response.status()).toBe(200);
+
+    const text = await response.text();
+
+    expect(text).toContain('morpheus');
+
+    console.log(await response.json());
+
+})
+
+test('API DELETE Request', async ({ request }) => {
+
+    const response = await request.delete('https://reqres.in/api/users/2', {
+        headers: {
+            "x-api-key": "reqres-free-v1"
+        }
+    });
+
+    expect(response.status()).toBe(204);
+
+})
